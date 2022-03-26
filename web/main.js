237e9946b837d25f5a87dfb4b49801e70b161f5a
Isidoro Legido Martínez <islegmar@gmail.com>
2022-03-26 16:03:18 +0100

    Test web (probably CORS problems)

---
+++ web/main.js
@@ -0,0 +1,31 @@
+function  getURLParam(key) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  return urlParams.get(key);
+}
+
+function fetchJSONFile(path, callback) {
+  console.log("url:" + path);
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200 || httpRequest.status === 0) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      } else {
+        alert("ERROR : " + httpRequest.status + " : ·" + httpRequest.responseText);
+      }
+    }
+  };
+  httpRequest.open('GET', path);
+  httpRequest.send(); 
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var url=getURLParam("url");
+  if ( !url ) url="https://drive.google.com/uc?export=download&id=17km7yRqEAKCyDi9adGeiTcaS--JQl3zJ";
+  fetchJSONFile(url, data => {
+    alert(data);
+  });
+});
